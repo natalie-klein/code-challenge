c868bbf285bdc694fea957dc953eafcbd661589e
NatalieKlein <natklein3@gmail.com>
2019-10-02 17:58:12 -0700

    initial setup, dev server working, error with font-src for prod server

---
+++ client/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import Column from './Column.js'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
